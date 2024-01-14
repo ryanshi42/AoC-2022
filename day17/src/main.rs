2115988319db72dc66e5fdc3fa3b151b154bc25d
Ryan Shi <ryan.shi42@gmail.com>
2024-01-14 19:13:42 +1100

    Unfinished day17, too much parsing.

--- day17/src/main.rs
+++ day17/src/main.rs
@@ -1,7 +1,7 @@
 
 // https://github.com/LinAGKar/advent-of-code-2023-rust/blob/master/day22/src/main.rs
 
-use std::{fs, collections::HashMap};
+use std::fs;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 enum BlockType {
@@ -23,7 +23,7 @@ impl BlockType {
     fn next_block(&self) -> BlockType {
         match self {
             BlockType::Minus => BlockType::Plus,
-            BlockType::Plus => BlockType::Minus,
+            BlockType::Plus => BlockType::L,
             BlockType::L => BlockType::Eye,
             BlockType::Eye => BlockType::Square,
             BlockType::Square => BlockType::Minus
@@ -32,11 +32,11 @@ impl BlockType {
 
     fn default_width(&self) -> (usize, usize) {
         match self {
-            BlockType::Minus => (2, 6),
-            BlockType::Plus => (2, 5),
-            BlockType::L => (2, 5),
+            BlockType::Minus => (2, 5),
+            BlockType::Plus => (2, 4),
+            BlockType::L => (2, 4),
             BlockType::Eye => (3, 3),
-            BlockType::Square => (3, 3)
+            BlockType::Square => (3, 4)
         }
     }
 }
@@ -57,11 +57,13 @@ impl Block {
     fn push_down(&mut self) {
         self.height -= 1;
     }
-    fn would_collide(&self, heights: &Vec<usize>) -> bool {
+    fn would_collide(&mut self, heights: &Vec<usize>) -> bool {
         let (x, y) = self.width;
+        self.height += 1;
+        // println!("h = {:?}", self.height);
         match self.block_type {
             BlockType::Minus => heights[x] == self.height - 1 || heights[y] == self.height - 1 || heights[x + 1] == self.height - 1 || heights[x + 2] == self.height - 1,
-            BlockType::Plus => heights[x] == self.height - 2 || heights[y] == self.height - 2 || heights[x + 1] == self.height - 1,
+            BlockType::Plus => heights[x] == self.height || heights[y] == self.height || heights[x + 1] == self.height - 1,
             BlockType::L => heights[x] == self.height - 1 || heights[y] == self.height - 1 || heights[x + 1] == self.height - 1,
             BlockType::Eye => heights[x] == self.height - 1,
             BlockType::Square => heights[x] == self.height - 1 || heights[y] == self.height - 1
@@ -69,20 +71,21 @@ impl Block {
     }
 }
 
+// Cbs, use SET to make sure that you don't move into another block.
 fn part_1(input: &str) -> usize {
-    let mut curr_block = Block { block_type: BlockType::Minus, height: 3, width: BlockType::Minus.default_width() };
-    let mut heights = vec![[0; 7]];
+    let mut curr_block = Block { block_type: BlockType::Minus, height: 4, width: BlockType::Minus.default_width() };
+    let mut heights = vec![0; 7];
     let mut i = 1;
+    let mut su = 1;
     loop {
-        let jet = input.chars().nth(i % input.len()).unwrap();
-        if i == 4 {
-            break;
-        }
+        let jet = input.chars().nth((i - 1) % input.len()).unwrap();
         match jet {
             '<' => curr_block.push_left(),
             '>' => curr_block.push_right(),
             _ => (),
         }
+        println!("{i} {:?} {}", heights, jet);
+        curr_block.push_down();
         if curr_block.would_collide(&heights) {
             println!("colliding");
             let (x, _) = curr_block.width;
@@ -108,18 +111,20 @@ fn part_1(input: &str) -> usize {
                     heights[x] = curr_block.height + 1;
                     heights[x + 1] = curr_block.height + 1;
                 }
-                _ => ()
             }
 
             let next_block_type = curr_block.block_type.next_block();
-            curr_block = Block { block_type: next_block_type.clone(), height: *heights.iter().max().unwrap() + 3, width: next_block_type.default_width() };
+            su += 1;
+            if su == 2023 {
+                return *heights.iter().max().unwrap();
+            }
+            curr_block = Block { block_type: next_block_type.clone(), height: *heights.iter().max().unwrap() + 4, width: next_block_type.default_width() };
         } else {
-            curr_block.push_down();
-
+            curr_block.height -= 1;
         }
         i += 1;
     }
-    *heights.last().unwrap().iter().max().unwrap()
+    *heights.iter().max().unwrap()
 }
 
 fn part_2(input: &str) -> usize {
