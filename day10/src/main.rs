df29807e7ddbf54c7fa58745378de70dc3e2bf69
Ryan Shi <ryan.shi42@gmail.com>
2024-01-15 15:36:20 +1100

    strip prefix + day 21.

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -2,6 +2,7 @@ use std::{io::Read, collections::{HashMap, HashSet}, fs};
 
 // https://github.com/LinAGKar/advent-of-code-2023-rust/blob/master/day22/src/main.rs
 
+// day 25
     // // 16616892
     // // 14505727
     // // 16616892
@@ -30,46 +31,164 @@ use std::{io::Read, collections::{HashMap, HashSet}, fs};
     // }
     // curr
 
-fn part_1(input: &str) -> u128 { 
-    let mut v: Vec<_> = input.lines().map(|x| x.parse::<u128>().unwrap()).collect();
-    v.sort();
-    let mut ones = 1;
-    let mut threes = 1;
-    for i in 1..v.len() {
-        if v[i] - v[i - 1] == 1 {
-            ones += 1;
-        } else if v[i] - v[i - 1] == 3 {
-            threes += 1;
-        } else {
-            panic!("at the disco");
-        } 
+// day 10
+    // let mut v: Vec<_> = input.lines().map(|x| x.parse::<u128>().unwrap()).collect();
+    // v.sort();
+    // let mut ones = 1;
+    // let mut threes = 1;
+    // for i in 1..v.len() {
+    //     if v[i] - v[i - 1] == 1 {
+    //         ones += 1;
+    //     } else if v[i] - v[i - 1] == 3 {
+    //         threes += 1;
+    //     } else {
+    //         panic!("at the disco");
+    //     } 
+    // }
+    // ones * threes
+
+// day 10 part 2
+    // let mut v: Vec<_> = input.lines().map(|x| x.parse::<usize>().unwrap()).collect();
+    // v.sort();
+
+    // let mut dp = vec![0 as usize; *v.last().unwrap() + 1];
+    // dp[0] = 1;
+    
+    // for i in v.clone().into_iter() {
+    //     dp[i] += dp[i - 1];
+    //     if i >= 2 {
+    //         dp[i] += dp[i - 2];
+    //     }
+    //     if i >= 3 {
+    //         dp[i] += dp[i - 3];
+    //     }
+    // }
+    // println!("{:?}", dp);
+    // dp[*v.last().unwrap()]
+
+fn part_1(input: &str) -> usize { 
+    // let allergens = HashSet::from_iter(["shellfish", "dairy", "soy", "nuts", "fish", "sesame", "wheat", "eggs", "sesame", "peanuts"]);
+    let mut allergens = HashSet::new();
+    let mut ingreds = HashSet::new();
+    let mut poss: HashMap<&str, HashSet<&str>> = HashMap::new();
+    let mut hm = HashMap::new();
+    for line in input.lines() {
+        // println!("{:?}", line);
+        let (imp, rest) = line.split_once(" (contains ").unwrap();
+        let here = rest[..rest.len()-1].split(", ").collect::<Vec<&str>>();
+        for allergen in here {
+            allergens.insert(allergen);
+        }
+
+        let ingred = imp.split(" ").collect::<Vec<&str>>();
+        for i in ingred {
+            ingreds.insert(i);
+
+            *hm.entry(i).or_insert(0) += 1;
+        }
     }
-    ones * threes
+
+    for a in allergens.iter() {
+        poss.insert(a, ingreds.clone());
+    }
+
+    for line in input.lines() {
+        let (imp, rest) = line.split_once(" (contains ").unwrap();
+        let here = rest[..rest.len()-1].split(", ").collect::<Vec<&str>>();
+
+        let ingred = imp.split(" ").collect::<Vec<&str>>();
+        let l = HashSet::from_iter(ingred);
+        let not_ingred = ingreds.difference(&l);
+        for allergen in here {
+            for i in not_ingred.clone() {
+                poss.get_mut(&allergen).unwrap().remove(i);
+            }
+        }
+
+    }
+
+    println!("{:?}", poss);
+
+    ingreds
+        .iter()
+        .filter(|i| {
+            for a in poss.values() {
+                if a.contains(**i) {
+                    return false;
+                }
+            }
+            true
+        })
+        .map(|x| hm.get(x).unwrap())
+        .sum()
 }
 
 fn part_2(input: &str) -> usize {
-    let mut v: Vec<_> = input.lines().map(|x| x.parse::<usize>().unwrap()).collect();
-    v.sort();
-
-    let mut dp = vec![0 as usize; *v.last().unwrap() + 1];
-    dp[0] = 1;
-    
-    for i in v.clone().into_iter() {
-        dp[i] += dp[i - 1];
-        if i >= 2 {
-            dp[i] += dp[i - 2];
+    let mut allergens = HashSet::new();
+    let mut ingreds = HashSet::new();
+    let mut poss: HashMap<&str, HashSet<&str>> = HashMap::new();
+    let mut hm = HashMap::new();
+    for line in input.lines() {
+        // println!("{:?}", line);
+        let (imp, rest) = line.split_once(" (contains ").unwrap();
+        let here = rest[..rest.len()-1].split(", ").collect::<Vec<&str>>();
+        for allergen in here {
+            allergens.insert(allergen);
         }
-        if i >= 3 {
-            dp[i] += dp[i - 3];
+
+        let ingred = imp.split(" ").collect::<Vec<&str>>();
+        for i in ingred {
+            ingreds.insert(i);
+
+            *hm.entry(i).or_insert(0) += 1;
         }
     }
-    println!("{:?}", dp);
-    dp[*v.last().unwrap()]
+
+    for a in allergens.iter() {
+        poss.insert(a, ingreds.clone());
+    }
+
+    for line in input.lines() {
+        let (imp, rest) = line.split_once(" (contains ").unwrap();
+        let here = rest[..rest.len()-1].split(", ").collect::<Vec<&str>>();
+
+        let ingred = imp.split(" ").collect::<Vec<&str>>();
+        let l = HashSet::from_iter(ingred);
+        let not_ingred = ingreds.difference(&l);
+        for allergen in here {
+            for i in not_ingred.clone() {
+                poss.get_mut(&allergen).unwrap().remove(i);
+            }
+        }
+    }
+
+    let mut mapping = HashMap::new();
+
+
+    for _ in 0..8 {
+        let mut who = "allergens.iter().next().unwrap();";
+        for (name, which) in poss.clone().iter() {
+            if which.len() == 1 {
+                who = which.iter().next().unwrap();
+                mapping.insert(*name, who);
+            }
+        }
+        for name in poss.clone().keys().clone() {
+            poss.get_mut(name).unwrap().remove(who);
+        }
+    }
+
+    println!("{:?}", mapping);
+    let mut ans: Vec<_> = mapping.into_iter().collect(); 
+    ans.sort_by(|a, b| a.0.cmp(b.0));
+    
+    println!("{:?}", ans.iter().map(|x| x.1).collect::<Vec<_>>().join(","));
+    0
 }
 
 fn main() {
     
-    let input = fs::read_to_string("src/day10.txt").unwrap();
+    let input = fs::read_to_string("src/day21.txt").unwrap();
 
     let start_time = std::time::Instant::now();
     let result = part_1(&input);
