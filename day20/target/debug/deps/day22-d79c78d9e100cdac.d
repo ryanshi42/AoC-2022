a187014c59a06b217ca921c2a6ee2ace775077af
Ryan Shi <ryan.shi42@gmail.com>
2024-01-15 17:20:59 +1100

    test commt

--- day20/target/debug/deps/day22-d79c78d9e100cdac.d
+++
@@ -1,5 +0,0 @@
-/Users/ryanshi/Desktop/Nothing/Uni/Comp/playground/AOC_2022/day22/target/debug/deps/libday22-d79c78d9e100cdac.rmeta: src/main.rs
-
-/Users/ryanshi/Desktop/Nothing/Uni/Comp/playground/AOC_2022/day22/target/debug/deps/day22-d79c78d9e100cdac.d: src/main.rs
-
-src/main.rs:
